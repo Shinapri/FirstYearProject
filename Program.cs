9c34a8e605512be5547a5de6d1589eb230acffa8
Shinapri <114340471+Shinapri@users.noreply.github.com>
2022-09-25 18:59:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine();
+            Console.Write("Number 1 : ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Number 2 : ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The Summation of {0} + {1} = {2}", num1, num2, num1 + num2);
+        }
+    }
+}
