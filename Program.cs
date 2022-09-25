fde08d965f236282d19b437dde25203c173fd6b6
Shinapri <114340471+Shinapri@users.noreply.github.com>
2022-09-25 19:10:10 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine();
+            Console.Write("Number 1 : ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Number 2 : ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("The Result of {0} + {1} = {2}", num1, num2, (num1 + num2).ToString("#.###"));
+            Console.WriteLine("The Result of {0} - {1} = {2}", num1, num2, (num1 - num2).ToString("#.###"));
+            Console.WriteLine("The Result of {0} x {1} = {2}", num1, num2, (num1 * num2).ToString("#.###"));
+            Console.WriteLine("The Result of {0} / {1} = {2}", num1, num2, (num1 / num2).ToString("#.###"));
+            Console.WriteLine("The Result of {0} % {1} = {2}", num1, num2, (num1 % num2).ToString("#.###"));
+            Console.WriteLine("The Result of {0} ^ {1} = {2}", num1, num2, (Math.Pow(num1,num2).ToString("#.###")));
+        }
+    }
+}
